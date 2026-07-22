90d7f3d370ad907f19cb2911a1198b630d1dc194
Ashish863 <ashishshukla203100@gmail.com>
2026-07-22 17:48:24 +0530

    default url

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import axios from 'axios'
 axios.defaults.withCredentials=true
+axios.defaults.baseURL="https://social-app-backend-0r47.onrender.com"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
